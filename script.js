2281cb9ad95a0accbcb8458978c98c5c7afa04e9
Laura Mendez <lauramendez@Lauras-Air.lan>
2023-03-12 07:42:22 -0700

    defining variables

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
  
-var letterArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',]; 
+var lowerChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 var lengthOfPassword = parseInt(window.prompt('Password Lenght'));
 
@@ -10,13 +14,15 @@ console.log(typeof lengthOfPassword);
 
 var result = [ ];
 
-
 for (var i = 0; i < lengthOfPassword; i++) {
-  var randomNum = numberArr[Math.floor(Math.random() * numberArr.lentgh)];
+  var randomNum = numberArr[Math.floor(Math.random() * number.length)];
   result.push (randomNum);
   console.log(result);
 
 }
+
+
+// all this was here//
 
 //Get reference to the #generate element
 var generateBtn = document.querySelector("#generate");
